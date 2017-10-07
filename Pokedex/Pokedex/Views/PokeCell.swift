f197eae93520115e30245e010ec494b23b5d4af6
Taangh <30960806+Taangh@users.noreply.github.com>
2017-10-08 00:38:07 +0200

    pokenames itd

---
+++ Pokedex/Pokedex/Views/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Damian on 07.10.2017.
+//  Copyright © 2017 Damian. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
